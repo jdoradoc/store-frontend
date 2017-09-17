5be113a5fda197ef85217d0436a858f1ed5514e0
jdoradoc <juan.dorado@ymail.com>
2017-09-17 21:04:03 +0200

    Add files via upload

--- fullstack-task/codingtask/src/main/java/de/rindus/codingtask/product/ProductController.java
+++ fullstack-task/codingtask/src/main/java/de/rindus/codingtask/product/ProductController.java
@@ -1,7 +1,11 @@
 package de.rindus.codingtask.product;
 
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
@@ -10,17 +14,22 @@ import java.util.List;
 import javax.validation.constraints.NotNull;
 
 
+
 @RestController
 @RequestMapping(path = "store")
 @CrossOrigin(origins = {"http://localhost:4200"})
 public class ProductController {
+	
+	private static final Logger LOGGER = LogManager.getLogger(ProductController.class.getName());
 
     @Autowired
     private ProductRepository productRepository;
 
     @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
     @RequestMapping("search/{filter}")
-    public List<Product> search(@PathVariable String filter) {
+    public ResponseEntity<List<Product>> search(@PathVariable String filter) {
+    	
+    	LOGGER.info("Search [filter: " + filter + "]");
 
     	List<Product> lstProducts = new ArrayList<Product>();
     	
@@ -31,32 +40,39 @@ public class ProductController {
     	}
 		
     	lstProducts.sort(Comparator.comparing(Product::getName));
-    	return lstProducts;
+    	return new ResponseEntity<List<Product>>(lstProducts, HttpStatus.OK);
 
     }
     
     @PostMapping("product/update")
-	public Product updateProduct(@RequestBody @NotNull Product pto) {
+	public ResponseEntity<Product> updateProduct(@RequestBody @NotNull Product pto) {
     	
     	if (pto.getId() != null) {
-    		System.out.println("Update: " + pto.getId());
+    		LOGGER.info("Update: " + pto.getId());
     		Product updatedProduct = productRepository.findOne(pto.getId());
     		updatedProduct.setName(pto.getName());
     		updatedProduct.setAmount(pto.getAmount());
     		updatedProduct.setCategory(pto.getCategory());
-    		return productRepository.save(updatedProduct);
+    		updatedProduct = productRepository.save(updatedProduct);
+    		return new ResponseEntity<Product>(updatedProduct, HttpStatus.OK);
     	}
-    	System.out.println("Save");
-        return productRepository.save(pto);
+    	
+    	LOGGER.info("Create: " + pto.getName());
+    	Product newProduct = productRepository.save(pto);
+        return new ResponseEntity<Product>(newProduct, HttpStatus.OK);
 	}
     
     @GetMapping("product/detail/{id}")
-    public Product detailProduct(@PathVariable("id") @NotNull String id) {
-    	return productRepository.findOne(id);
+    public ResponseEntity<Product> detailProduct(@PathVariable("id") @NotNull String id) {
+    	LOGGER.info("Detail: " + id);
+    	Product detail = productRepository.findOne(id);
+    	return new ResponseEntity<Product>(detail, HttpStatus.OK);
     }
     
     @DeleteMapping("product/delete/{id}")
-	public void deleteProduct(@PathVariable("id") @NotNull String id) {
+	public ResponseEntity<Void> deleteProduct(@PathVariable("id") @NotNull String id) {
+    	LOGGER.info("Delete: " + id);
    		productRepository.delete(id);
+   		return new ResponseEntity<Void>(HttpStatus.NO_CONTENT);
 	}    
 }
